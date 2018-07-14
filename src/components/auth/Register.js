8f533d13c2948b95c1ccc5a0f181c7985537fd4a
dbarlev <dbarlev1@gmail.com>
2018-07-15 00:36:52 +0300

    add register component

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,33 +1,144 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import { Button, FormGroup, FormControl } from 'react-bootstrap';
+import {
+  Button,
+  Form,
+  FormGroup,
+  FormControl,
+  Col,
+  Checkbox,
+  ControlLabel
+} from 'react-bootstrap';
 import Header from '../header/Header.js'
 
 class Register extends Component {
 
- constructor()
- {
+  constructor()
+  {
     super();
+  }
+
+  populatePackagesCombobox() {
+    let value = this.props.chooseSubscription.value;
+    let feild;
+    if (this.props.chooseSubscription.value != false) {
+      switch (value) {
+        case 1:
+          feild = <FormControl disabled="true" value="חבילת חינם"/>
+          break;
+        case 2:
+          feild = <FormControl disabled="true" value="חבילת פרימיום"/>
+          break;
+        case 3:
+          feild = <FormControl disabled="true" value="חבילת סופר פרימיום"/>
+          break;
+      }
+
+      return (
+        <div>
+          <FormGroup controlId="formHorizontalPackage">
+            <ControlLabel>בחר חבילה:</ControlLabel>
+            <Col sm={8}>
+              {feild}
+            </Col>
+          </FormGroup>
+        </div>
+      )
+    } else {
+      return (
+        <div>
+          <FormGroup controlId="formHorizontalPackage">
+            <ControlLabel>בחר חבילה:</ControlLabel>
+            <Col sm={8}>
+              <FormControl componentClass="select">
+                <option value="1">חבילת חינם</option>
+                <option value="2">חבילת פרימיום</option>
+                <option value="3">חבילת סופר פרימיום</option>
+              </FormControl>
+            </Col>
+          </FormGroup>
+        </div>
+      )
+    }
   }
 
   render() {
 
     return (
-        <div className="App">
-            <Header headline="הרשמה"/>
-            <span>חבילה מסוג: </span>
-            <span>{this.props.chooseSubscription.name}</span>
+      <div className="App" id="register">
+        <Header headline="הרשמה"/>
+        <span>חבילה מסוג:
+        </span>
+        <span>{this.props.chooseSubscription.name}</span>
+        <div className="row">
+          <div className="col-md-4 col-md-offset-4">
+            <Form horizontal>
+              <FormGroup controlId="formHorizontalEmail">
+                <Col sm={8}>
+                  <FormControl type="email" placeholder="הקלד דואר אלקטרוני"/>
+                </Col>
+                <Col componentClass={ControlLabel}>
+                  דואר אלקטרוני:
+                </Col>
+              </FormGroup>
+
+              <FormGroup controlId="formHorizontalUserName">
+                <Col sm={8}>
+                  <FormControl type="text" placeholder="הקלד שם משתמש"/>
+                </Col>
+                <Col componentClass={ControlLabel}>
+                  שם משתמש:
+                </Col>
+
+              </FormGroup>
+
+              <FormGroup controlId="formHorizontalPassword">
+                <Col sm={8}>
+                  <FormControl type="password" placeholder="הקלד סיסמה"/>
+                </Col>
+                <Col componentClass={ControlLabel}>
+                  סיסמה:
+                </Col>
+              </FormGroup>
+
+              <FormGroup controlId="formConfirmPassword">
+                <Col sm={8}>
+                  <FormControl type="password" placeholder="הקלד סיסמה שנית"/>
+                </Col>
+                <Col componentClass={ControlLabel}>
+                  וודא סיסמה:
+                </Col>
+              </FormGroup>
+
+              {this.populatePackagesCombobox()}
+
+              <FormGroup>
+                <Col sm={12}>
+                  <Checkbox>
+                    אני מסכים לקבל עדכונים במייל
+                  </Checkbox>
+                </Col>
+              </FormGroup>
+
+              <FormGroup>
+                <Col >
+                  <Button type="submit">הירשם</Button>
+                </Col>
+              </FormGroup>
+
+            </Form>
+          </div>
         </div>
-        
+
+      </div>
+
     );
   }
 }
 
 const mapStateToProps = (state) => {
-    console.log("state",state)
-    return {
-       chooseSubscription: state.chooseSubscription
-    }
+  console.log("state", state)
+  return {chooseSubscription: state.chooseSubscription}
 }
 
 export default connect(mapStateToProps, {})(Register);
