5093b37d43d37f7cbc7b230527a91fc921da1a0b
David <davseveloff@gmail.com>
2018-10-22 11:01:02 +0300

    register componnent adds data to state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,71 +1,86 @@
-import { 
-     CHOOSE_SUBSCRIPTION,
-     DELETE_RECORD_FROM_USER,
-     GET_RECORDS_FROM_DB,
-     INSERT_RECORD_TO_USER,
-     SET_RETRIVED_DATE
+import {
+    CHOOSE_SUBSCRIPTION,
+    DELETE_RECORD_FROM_USER,
+    GET_RECORDS_FROM_DB,
+    INSERT_RECORD_TO_USER,
+    SET_RETRIVED_DATE,
+    ADD_USER
 } from '../actions/consts';
 
 import { populateBookApa, populatePaperApa, populateArticleApa, populateWebisteApa, formatRecordsToApa, populateAPAData } from './functions.js';
-import { combineReducers } from 'redux';     
+import { combineReducers } from 'redux';
 
 
-function chooseSubscription(state = [], action){
+function chooseSubscription(state = [], action) {
     switch (action.type) {
-      case CHOOSE_SUBSCRIPTION:
-        return {
-          value: action.value,
-          name: action.name
-        }
-    default:
-        return {
-          value: false,
-          name: "לא נבחרה חבילה"
-        }
-  }
+        case CHOOSE_SUBSCRIPTION:
+            return {
+                value: action.value,
+                name: action.name
+            }
+        default:
+            return {
+                value: false,
+                name: "לא נבחרה חבילה"
+            }
+    }
 }
- 
-function deleteRecordFromUser(state = [], action){
+
+function deleteRecordFromUser(state = [], action) {
     switch (action.type) {
         case DELETE_RECORD_FROM_USER:
-        return {
-            value: action.value,
-            name: "bookid"
-        }
-    default:
-        return state;
+            return {
+                value: action.value,
+                name: "bookid"
+            }
+        default:
+            return state;
     }
 }
 
-function getRecordsFromDB(state = [], action){
+function getRecordsFromDB(state = [], action) {
     switch (action.type) {
         case GET_RECORDS_FROM_DB:
-           //return formatRecordsToApa(action);
-           return populateAPAData(action);
+            //return formatRecordsToApa(action);
+            return populateAPAData(action);
         case INSERT_RECORD_TO_USER:
             //return formatRecordsToApa(action);
-           return populateAPAData(action);
-    default:
-        return state;
+            return populateAPAData(action);
+        default:
+            return state;
     }
 }
 
 
-function retrivedDate(state = [], action){
+function retrivedDate(state = [], action) {
     switch (action.type) {
         case SET_RETRIVED_DATE:
-           //return formatRecordsToApa(action);
-           return populateAPAData(action);
-    default:
-        return state;
+            //return formatRecordsToApa(action);
+            return populateAPAData(action);
+        default:
+            return state;
+    }
+}
+
+function userReducer(state = [], action) {
+    switch (action.type) {
+        case ADD_USER:
+            console.log('action user', action);
+            return [
+                action.user
+            ];
+
+        default:
+            return state;
     }
 }
 
 const rootReducer = combineReducers({
-     chooseSubscription,
-     deleteRecordFromUser,
-     getRecordsFromDB,
-     retrivedDate
+    chooseSubscription,
+    deleteRecordFromUser,
+    getRecordsFromDB,
+    retrivedDate,
+    userReducer
 });
 
 export default rootReducer;
