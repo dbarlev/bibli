02a8499b992af3b99bd6be63127856809cce24ca
David <davseveloff@gmail.com>
2018-10-31 21:33:51 +0200

    new footer  form

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import Header from './header/Header.js';
 import SubOptions from './subscription/SubOptions';
 import Search from './bib/search/Search';
 import BibRecordsList from './bib/records/BibRecordsList';
-
+import Footer from './footer/Footer'
 import './App.css';
 
 class App extends Component {
@@ -13,6 +13,7 @@ class App extends Component {
         <Header headline="1ברוכים הבאים לביבלי"/>
         <SubOptions />
         <br />
+        <Footer />
       </div>
     );
   }
