663a933ee99fa1e4835c8bf31a1a2ad1d9d66aee
dbarlev <dbarlev1@gmail.com>
2019-06-15 12:58:34 +0300

    home page design

--- src/components/frontpage/PricingTable.js
+++ src/components/frontpage/PricingTable.js
@@ -1,67 +1,109 @@
 import React, { Component } from 'react';
-import { Image, Row } from 'react-bootstrap';
+import { Image, Row, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import researcher from '../img/researcher.png';
 import student from '../img/student.png';
 import guest from '../img/guest.png';
 
+const _Yearly = "year";
+const _Monthly = "month";
+
+
 export class PricingTable extends Component {
+
+    
+  constructor(){
+    super();
+    this.state = {
+        price:{
+            month:{
+                guest: 0,
+                student: 20,
+                investigator: 50
+            },
+            year:{
+                guest: 0,
+                student: 8,
+                investigator: 45
+            }          
+        },
+        priceType: _Yearly
+    }
+  }  
+
   render() {
+
+    let { price, priceType } = this.state;
+    price = price[priceType];
+
     return (
       <div id="pricing-table">
       <h2 className="text-center big-title white p30top">התוכניות שלנו</h2>
 
         <ul>
-            <li className="text-center">
-                <Image src={researcher} alt="משתמש מסוג אורח"/>
+            <li className="text-center price-regular">
+                <Image src={guest} alt="משתמש מסוג אורח"/>
                 <Row>
-                    <span className="big-text large">ללא</span>
+                    <span className="large">ללא</span>
                     <br />
-                    <span className="big-text large">עלות</span>
+                    <span className="large">עלות</span>
                 </Row>
 
                 <Row>
-                    <p className="large">רשומות ביבליוגרפיות:</p>
+                    <p className="large bold">רשומות ביבליוגרפיות:</p>
                     <p>1</p>
                 </Row>
                 <Row>
-                    <p className="large">פריטים ביבליוגרפיים:</p>
+                    <p className="large bold">פריטים ביבליוגרפיים:</p>
                     <p>7</p>
                 </Row>
+                <Row>
+                    <Button bsStyle="primary" className="btn-yellow blue-text"> <Link to="/register"> לבחירת התכנית <i className="fas fa-chevron-left btn-yellow"></i></Link></Button>
+                </Row>
             </li>
-            <li className="text-center">
-                <Image src={researcher} alt="משתמש מסוג סטודנט"/>
-                <Row>
-                    <span className="big-text large">8</span>
-                    <span className="smallNum large">ש"ח</span>
-                    <br />
-                    <span className="bigNum">לחודש</span>
-                </Row>
+            <li className="text-center price-recomended">
+                <div className="recomended-badge">הנבחרת ביותר</div>
+                <div className="recomended-container" >
+                    <Image src={student} alt="משתמש מסוג סטודנט"/>
+                    <Row>
+                        <span className="smallNum">₪</span>
+                        <span className="large bold">{price.student}</span>
+                        <br />
+                        <span className="bigNum">לחודש</span>
+                    </Row>
 
-                <Row>
-                    <p className="large bold">רשומות ביבליוגרפיות:</p>
-                    <p className="bold">200</p>
-                </Row>
-                <Row>
-                    <p className="large bold">פריטים ביבליוגרפיים:</p>
-                    <p className="bold">ללא הגבלה</p>
-                </Row>
+                    <Row>
+                        <p className="large bold">רשומות ביבליוגרפיות:</p>
+                        <p className="large">200</p>
+                    </Row>
+                    <Row>
+                        <p className="large bold">פריטים ביבליוגרפיים:</p>
+                        <p className="large">ללא הגבלה</p>
+                    </Row>
+                    <Row>
+                        <Button bsStyle="primary"> <Link to="/register"> לבחירת התכנית <i className="fas fa-chevron-left"></i></Link></Button>
+                    </Row>
+                </div>
             </li>    
-            <li className="text-center">
+            <li className="text-center price-regular">
                 <Image src={researcher} alt="משתמש מסוג חוקר"/>
                 <Row>
-                    <span className="big-text large">45</span>
-                    <span className="smallNum">ש"ח</span>
+                    <span className="smallNum bold">₪</span>
+                    <span className="large bold">{price.investigator}</span>
                     <br />
                     <span className="bigNum">לחודש</span>
                 </Row>
 
                 <Row>
                     <p className="large bold">רשומות ביבליוגרפיות:</p>
-                    <p className="bold">ללא הגבלה</p>
+                    <p className="large">ללא הגבלה</p>
                 </Row>
                 <Row>
                     <p className="large bold">פריטים ביבליוגרפיים:</p>
-                    <p className="bold">ללא הגבלה</p>
+                    <p className="large">ללא הגבלה</p>
+                </Row>
+                <Row>
+                    <Button bsStyle="primary" className="btn-yellow blue-text"> <Link to="/register"> לבחירת התכנית <i className="fas fa-chevron-left btn-yellow"></i></Link></Button>
                 </Row>
             </li>
         </ul>
