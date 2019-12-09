b8b936fd704d95c0acee0ca2192f14ae8f91cb75
dbarlev <dbarlev1@gmail.com>
2019-12-09 19:05:22 +0200

    aaa

--- src/components/header/LogOut.js
+++ src/components/header/LogOut.js
@@ -1,22 +1,21 @@
 import React, { Component } from 'react'
-import { Redirect  } from 'react-router-dom';
+import { Redirect } from 'react-router-dom';
 import { MenuItem, Button } from 'react-bootstrap';
-import { LinkContainer, IndexLinkContainer  } from "react-router-bootstrap";
+import { LinkContainer, IndexLinkContainer } from "react-router-bootstrap";
 import { connect } from 'react-redux';
 import { userLogedIn } from '../../actions';
 
 
-function eraseCookie(name) {   
-    document.cookie = name+'=; Max-Age=-99999999;';  
+function eraseCookie(name) {
+    document.cookie = name + '=; Max-Age=-99999999;';
 }
 
 
 
 export class LogOut extends Component {
-    logOut = () => {
+    logOut() {
         eraseCookie('auth')
         eraseCookie('userid')
-
         this.props.userLogedIn();
         return <Redirect to='/' />
     }
@@ -24,30 +23,24 @@ export class LogOut extends Component {
 
     render() {
 
-       
+
         return (
             <div>
                 <LinkContainer className="btn-warning black topnav-login-logout-btn" style={TopMargin} to="/records/biblist" >
-                <div>חזרה לאזור האישי</div>
+                    <div>חזרה לאזור האישי</div>
                 </LinkContainer>
                 <LinkContainer className="btn-warning black topnav-login-logout-btn" style={TopMargin} to="/" >
-                    <Button onClick={this.logOut}> התנתק/י</Button>
+                    <Button onClick={() => this.logOut()}> התנתק/י</Button>
                 </LinkContainer>
             </div>
         )
     }
 }
 
-const mapDispatchToProps = dispatch => {
-    return {
-        userLogedIn: (params) => dispatch(userLogedIn(params))
-    };
-};
 
-
-export default connect(null, mapDispatchToProps)(LogOut);
+export default connect(null, { userLogedIn })(LogOut);
 
 
 const TopMargin = {
     marginTop: "27px"
-  };
+};
