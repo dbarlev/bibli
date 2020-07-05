e5d95ae5eb0c5a86d137dfa93b405e1fc8ece75a
David <davseveloff@gmail.com>
2020-07-05 16:15:12 +0300

    lopp and single works

--- src/components/pages/BlogSingle.js
+++ src/components/pages/BlogSingle.js
@@ -7,43 +7,45 @@ import Header from "../header/Header";
 import Footer from "../footer/Footer";
 import { getSinlePostFromWP } from "../../actions/ajax";
 import StickyContact from '../sticky/stickyContact/StickyContact';
+import './BlogSingle.scss';
 
 class BlogSingle extends Component {
- constructor(props){
-     super(props);
-
-
- }
-
+    constructor(props){
+        super(props);
+   
+   
+    }
     
  componentDidMount() {
+    
     this.props.getSinlePostFromWP(this.props.match.params.postSlug);
     console.log('the post slug is ', this.props.match.params.postSlug);
 }
 
-
-//     wpPosts() {
-//         console.log('props ', this.props)
-       
-    
-//     return posts
-// }
     render() {
-        
-        const post = this.props.post
+        const post =this.props.post;
         return (
+            
             <Grid fluid>
                 <Header />
                 <main id="aboutUs-main">
                     <Row>
                         <Col md={6} mdOffset={3}>
-                            <h1>{console.log('props', this.props.post)} </h1>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col md={6} mdOffset={3}>
-                          {/* {posts} */}
-                        </Col>
+                       
+                        {
+                            this.props.post &&
+                            <div>
+                            <h1>{ReactHtmlParser(post[0].title.rendered)} </h1>
+                            <img style={image} alt="aa" src="http://blog.bibli.co.il/wp-json/wp/v2/media/8" />
+                            <p>{console.log('post', post)}</p>
+
+                            <div>{ReactHtmlParser(post[0].content.rendered)} </div>
+
+                            </div>
+                           
+                            
+                        }
+                            </Col>
                     </Row>
                 </main>
                 <StickyContact />
@@ -52,6 +54,9 @@ class BlogSingle extends Component {
         );
     }
 }
+
+
+
 const mapStateToProps = state => {
     return{
      post: state.PostsFromWPReducer.post
@@ -59,3 +64,17 @@ const mapStateToProps = state => {
 }
 export default connect(mapStateToProps, {getSinlePostFromWP})(BlogSingle);
 
+const footerX = {
+    position: 'absolute',
+    bottom: '0',
+    minWidth: '100%',
+    padding: '0',
+    margin: '0',
+    left: '0',
+}
+
+const image = {
+    width: '100%'
+  };
+  
+  
